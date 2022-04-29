136e39b9eb84408a54f714be792f6d852f89ae5c
nomore <nightwing@naver.com>
2022-04-29 13:29:04 +0900

    help center 레이아웃 수정

--- src/views/components/helpCenter/Sidebar.tsx
+++ src/views/components/helpCenter/Sidebar.tsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import { Link, NavLink, useLocation, useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { TABLET_SIZE } from "@/assets/styles/responsiveBreakPoint";
 
 const Sidebar = () => {
-  const location = useLocation();
-
-  // pathname에 의존성이 있어서 좋은 방법인지는 잘...
-  useEffect(() => {
-    // const query = `.hs-${location.pathname.replace("/", "")}`;
-    // const menu = document.querySelector(query);
-    // menu?.classList.add("menu-active");
-    // console.log("render");
-  }, []);
-
   return (
     <SidebarStyle>
       <h3 className="helpCenterNav-title">Help Center</h3>
@@ -47,8 +38,9 @@ export default Sidebar;
 const SidebarStyle = styled.aside`
   width: 160px;
   height: 240px;
+  position: absolute;
 
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: ${TABLET_SIZE}) {
     display: none;
   }
 
@@ -75,56 +67,3 @@ const SidebarStyle = styled.aside`
     }
   }
 `;
-
-/**
- * --------------------------------------------------------------------------
- * deposit, withdraw 페이지 공통
- * - t113C에서 szBankAccNo 가져와야함
- *   t113C 로그인할때 사용됨
- *
- * deposit
- * - t2313에서 deposit list
- *
- * withdraw
- * - t2413에서 withdraw list
- *
- * --------------------------------------------------------------------------
- *
- * submit request
- * - t2511에서 게시글 리스트
- *     const faqListInfo = {
- *       Header: {
- *       	function: 'D', termtype: 'HTS', trcode: 't2511'
- *   	},
- *       	Input1: { szCust_No: email, szFrom_Date: '20220109000000', szTo_Date: now },
- *     };
- *
- * submit request detail page
- * - t2713 게시글 디테일
- *     const info = {
- *       Header: {
- *         function: 'D',
- *         termtype: 'HTS',
- *         trcode: 't2713',
- *       },
- *       Input1: {
- *         szCust_No: name,
- *         szReq_Seq_No: id,
- *         szPrc_Seq_No: '1',
- *       },
- *     };
- *
- * submit request request
- * - t2510 게시글 요청
- * {
- *     Header: { function: 'D', termtype: 'HTS', trcode: 't2510' },
- *     Input1: {
- *       szCust_No: email,
- *       szReq_Seq_No: '0',
- *       szQue_Title_Data: e.target.value,
- *       szQue_Data1: '',
- *       szQue_Data2: '',
- *       szQue_Data3: '',
- *       szQue_Memo: content,
- *     },
- * */
