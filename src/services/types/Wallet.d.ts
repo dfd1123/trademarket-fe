37d9e8c57f88a3610f6a1acd73278fe49d1c8957
dfd1123 <dfd1123@naver.com>
2022-05-04 01:08:33 +0900

    future trade 구현 완료

--- src/services/types/Wallet.d.ts
+++ src/services/types/Wallet.d.ts
@@ -5,26 +5,35 @@ export interface AssetData {
 }
 
 export interface CurrentCoinInfo {
-    fBuyPrice: number;
-    fClose: number;
-    fHigh: number;
-    fLow: number;
-    fOpen: number;
-    fPreClose: number;
-    fSellPrice: number;
-    fVolume: number;
-    szCurNo: string;
-    szDate: string;
-    szTime: string;
+  fBuyPrice: number;
+  fClose: number;
+  fHigh: number;
+  fLow: number;
+  fOpen: number;
+  fPreClose: number;
+  fSellPrice: number;
+  fVolume: number;
+  szCurNo: string;
+  szDate: string;
+  szTime: string;
 }
 
 export interface WalletHistoryData {
-    txId: string;
-    symbol: string;
-    side: string;
-    orderType: string;
-    amount: number;
-    currentPrice: number;
-    date: string;
-    status: number;
+  txId: string;
+  symbol: string;
+  side: string;
+  orderType: string;
+  amount: number;
+  currentPrice: number;
+  date: string;
+  status: number;
+}
+
+export interface WalletFutureTradeHistoryData {
+  date: string;
+  no: number;
+  accountingCode: string;
+  point: number;
+  pointCurrentBal: number;
+  excutionNo: string;
 }
